1f130af6cc10ff2f00ee56352797810c14363100
Wafiq Tafazzul Hilmi <wafiqhilmi4@gmail.com>
2025-03-31 00:32:08 +0700

    Menambahkan Top App Bar

--- app/src/main/java/com/wafiqz0085/assesment1/MainActivity.kt
+++ app/src/main/java/com/wafiqz0085/assesment1/MainActivity.kt
@@ -1,47 +1,68 @@
-package com.wafiqz0085.assesment1
+    package com.wafiqz0085.assesment1
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.wafiqz0085.assesment1.ui.theme.Assesment1Theme
+    import android.content.res.Configuration
+    import android.os.Bundle
+    import androidx.activity.ComponentActivity
+    import androidx.activity.compose.setContent
+    import androidx.activity.enableEdgeToEdge
+    import androidx.compose.foundation.layout.padding
+    import androidx.compose.material3.ExperimentalMaterial3Api
+    import androidx.compose.material3.MaterialTheme
+    import androidx.compose.material3.Scaffold
+    import androidx.compose.material3.Text
+    import androidx.compose.material3.TopAppBar
+    import androidx.compose.material3.TopAppBarDefaults
+    import androidx.compose.runtime.Composable
+    import androidx.compose.ui.Modifier
+    import androidx.compose.ui.res.stringResource
+    import androidx.compose.ui.tooling.preview.Preview
+    import com.wafiqz0085.assesment1.ui.theme.Assesment1Theme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Assesment1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+    class MainActivity : ComponentActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
+            setContent {
+                Assesment1Theme {
+                    MainScreen()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Assesment1Theme {
-        Greeting("Android")
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun MainScreen() {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = stringResource(id = R.string.app_name))
+                    },
+                    colors = TopAppBarDefaults.mediumTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
+        ) { innerPadding ->
+            ScreenContent(Modifier.padding(innerPadding))
+        }
     }
-}
+
+    @Composable
+    fun ScreenContent( modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello Android!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+    @Composable
+    fun MainScreenPreview() {
+        Assesment1Theme {
+            MainScreen()
+        }
+    }
