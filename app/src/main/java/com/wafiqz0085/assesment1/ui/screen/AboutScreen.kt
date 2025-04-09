57feb91952f6c584cfee134e7111672318d5d7d2
Wafiq Tafazzul Hilmi <wafiqhilmi4@gmail.com>
2025-04-09 21:39:31 +0700

    Membuat navigasi dengan tombol Up

--- app/src/main/java/com/wafiqz0085/assesment1/ui/screen/AboutScreen.kt
+++ app/src/main/java/com/wafiqz0085/assesment1/ui/screen/AboutScreen.kt
@@ -2,7 +2,11 @@ package com.wafiqz0085.assesment1.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,15 +17,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.wafiqz0085.assesment1.R
 import com.wafiqz0085.assesment1.ui.theme.Assesment1Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutScreen() {
+fun AboutScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
             TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {navController.popBackStack()}) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
                 title = { Text(text = stringResource(R.string.tentang_aplikasi)) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -42,6 +58,6 @@ fun AboutScreen() {
 @Composable
 fun AboutScreenPreview() {
     Assesment1Theme {
-        AboutScreen()
+        AboutScreen(rememberNavController())
     }
 }
